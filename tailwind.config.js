612a76b2942fb8e558233cd197839db893f8a8e0
Mark Kevin Sandoval <sandoval.markkevin.j@gmail.com>
2023-09-03 12:12:11 +0800

    Fixed path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["*"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
       bangers: ["Bangers", "cursive"],
